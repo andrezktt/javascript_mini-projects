f7095e9a053134e5cdbf08178134c9b230f472e6
andre_zktt <andrezicatti@gmail.com>
2024-02-23 20:26:08 -0300

    23022024

---
+++ to-do-list/script.js
@@ -0,0 +1,49 @@
+const pushTask = document.querySelector('#push_task');
+const inputTask = document.querySelector('#input_task');
+const taskDetails = document.querySelector('#task_details');
+const tasks = document.querySelector('#tasks');
+const container = document.querySelector('.container');
+
+
+pushTask.addEventListener('click', (e) => {
+   if(inputTask.value.length == 0 ||taskDetails.value.length == 0) {
+      alert("Por avor, insira uma tarefa e seus detalhes ou etapas.");
+   } else {
+      tasks.innerHTML += `
+      <div class="task">
+         <div id="task_text">
+            <h2 id="taskname">
+               ${inputTask.value}
+            </h2>
+            <p id="details">
+               ${taskDetails.value}
+            </p>
+         </div>
+         <button class="delete">
+            <span class="material-symbols-outlined">
+               delete
+            </span>
+            <span> Apagar </span>
+         </button>
+      </div>`;
+      
+      inputTask.value = '';
+      taskDetails.value  = '';
+
+      let currentTask = document.querySelectorAll(".delete");
+      for(let i = 0; i < currentTask.length; i++) {
+         currentTask[i].onclick = function() {
+            this.parentNode.remove()
+         }
+      }
+
+      let taskText = document.querySelectorAll("#task_text")
+      for(let i = 0; i < taskText.length; i++) {
+         taskText[i].onclick = function() {
+            this.classList.toggle('completed')
+         }
+      }
+   }
+})
+
+
